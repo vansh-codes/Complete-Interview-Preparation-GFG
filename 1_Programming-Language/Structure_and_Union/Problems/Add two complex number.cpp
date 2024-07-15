7003c19350d839b3899b16067a08b49065f4face
Mananpoddar <119034136+manan307@users.noreply.github.com>
2024-07-16 00:51:42 +0530

    Create Add two complex number.cpp

---
+++ 1_Programming-Language/Structure_and_Union/Problems/Add two complex number.cpp
@@ -0,0 +1,8 @@
+// User function Template for C++
+
+Complex add(Complex c1, Complex c2) {
+    Complex result;
+    result.real = c1.real + c2.real;
+    result.img = c1.img + c2.img;
+    return result;
+}
